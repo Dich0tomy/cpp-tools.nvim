7566388e9c4362dd8516031bd59e9d6c6f494f3a
b4mbus <github@yonderly.addy.io>
2024-07-24 18:35:45 +0200

    test: add an automatic test runner
    This commit removes the `spec` folder and adds an `auto_test_runner.lua`
    file.
    
    The `.busted` file is configured to only scan for this file and run it.
    
    The auto test runner scans all the lua files, requires them and looks
    for an exposed `__test` function. If it finds one, it calls it with the
    global function environment (if we didn't override the fenv the `__test`
    functions wouldn't have access to busted and luassert functions).
    
    For modules which error upon `require` a special dummy test is made
    which prints the error message, thus failing the entire suite.
    
    This solution not only makes testing extremely flexible, it also allows
    for easy testing of private implementation.

--- .luacheckrc
+++ .luacheckrc
@@ -1,6 +1,6 @@
 std = "luajit"
 cache = true
-include_files = {"spec/**.lua", "lua/**.lua", "*.rockspec", ".luacheckrc"}
+include_files = {"lua/**.lua", "*.rockspec", ".luacheckrc"}
 
 read_globals = {
   "vim",
---
+++ lua/cpp-tools/auto_test_runner.lua
@@ -0,0 +1,45 @@
+--- This is a special file which gets called by busted.
+--- This file scans all the requirable `lua/` files from here
+--- and runs their __test functions with busted test context.
+
+local current_filename = debug.getinfo(1).source:match('([%w_%.]+)$')
+
+local function project_lua_files(path, type)
+	return type == 'file' and vim.endswith(path, 'lua') and not vim.endswith(path, current_filename)
+end
+
+local function path_to_requirable(path)
+	return path:match('lua/(.*)'):gsub('/init%.lua', ''):gsub('%.lua', ''):gsub('/', '.')
+end
+
+local function run_module_test(name)
+	local ok, module = pcall(require, name)
+	if not ok then
+		describe(name, function()
+			it('Has an error!', function()
+				assert.message(('Requiring this module failed with the following error:\n%s'):format(module)).truthy(false)
+			end)
+		end)
+		return
+	end
+
+	-- This module is something else, we don't fw it
+	if type(module) ~= 'table' then
+		return
+	end
+
+	local test_func = vim.tbl_get(module, '__test')
+	if test_func then
+		setfenv(test_func, getfenv())()
+	end
+end
+
+local function is_lua_dir(dir)
+	return vim.startswith(dir, 'lua')
+end
+
+vim
+	.iter(vim.fs.dir('.', { depth = 10, skip = is_lua_dir }))
+	:filter(project_lua_files)
+	:map(path_to_requirable)
+	:each(run_module_test)
