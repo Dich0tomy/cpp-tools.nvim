c64dbbf58ec5d222bb1664b96ef2d96e3ecfc731
b4mbus <github@yonderly.addy.io>
2024-08-07 23:19:05 +0200

    fix(test-runner): add proper support for testfiles

--- lua/cpp-tools/auto_test_runner/auto_test_runner.lua
+++ lua/cpp-tools/auto_test_runner/auto_test_runner.lua
@@ -3,7 +3,7 @@
 --- and runs their __test functions with busted test context.
 
 local current_filename = debug.getinfo(1).source:match('([%w_%.]+)$')
-local testfiles_dir = vim.fs.root(0, 'testfiles')
+local testfiles_dir = vim.fs.root(0, 'testfiles') .. '/testfiles'
 
 local function project_lua_files(path, type)
 	return type == 'file' and vim.endswith(path, 'lua') and not vim.endswith(path, current_filename)
--- testfiles/README.md
+++ testfiles/README.md
@@ -3,3 +3,5 @@
 A special directory with all kinds of test files categorized by modules that use them in their tests.
 
 Tests use these files by requiring them like so: `require('testfiles.lib.modules')`, `require('testfiles.modules.foo')`, etc.
+
+Or simply opened by doing `io.open(testfile_dir .. 'lib/modules/foo.txt')`
